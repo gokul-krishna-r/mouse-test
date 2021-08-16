6fea9d0d2f3af815cdaee9d3eee10f0aac252e19
Gokul <55547509+gokul-krishna-r@users.noreply.github.com>
2021-08-16 14:11:12 +0530

    feat: images changed to svg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import './App.css';
 import React,{useEffect, useState} from 'react';
-import Mouse from './images/mouse.png';
-import MouseRight from './images/mouse_right.png';
-import MouseLeft from './images/mouse_left.png';
-import MouseMiddle from './images/mouse_middle.png';
-import MouseUp from './images/mouse_wheel_up.png';
-import MouseDown from './images/mouse_wheel_down.png';
+import MouseSVG from './images/mouse.svg';
+import { SvgLoader, SvgProxy } from 'react-svgmt';
 
 
 
@@ -13,42 +9,50 @@ function App() {
   const [rightClicked,setRightClicked] = useState(false);
   const [leftClicked,setLeftClicked] = useState(false);
   const [middleClicked,setMiddleClicked] = useState(false);
-  const [clicked,setClicked] = useState(true);
+  const [forwardClicked,setForwardClicked] = useState(false);
+  const [backwardClicked,setBackwardClicked] = useState(false);
   const [wheelUp,setWheelUp] = useState(false);
   const [wheelDown,setWheelDown] = useState(false);
-
+  
+ 
   
   const handleMouseDown = (e)=>{
     e.preventDefault();
-    setClicked(false);
     if (typeof e === 'object') {
       switch (e.button) {
-        case 0:
+        case 0:{
           console.log('Left button clicked.');
           setLeftClicked(true);
           break;
-        case 1:
+        }
+        case 1:{
           console.log('Middle button clicked.');
           setMiddleClicked(true);
           break;
-        case 2:
+        }
+        case 2:{
           console.log('Right button clicked.');
           setRightClicked(true);
           break;
-        case 3:
+        }
+        case 3:{
           console.log('Backward button clicked.');
+          setBackwardClicked(true);
           break;
-        case 4:
+        }
+        case 4:{
           console.log('Forward button clicked.');
+          setForwardClicked(true);
           break;
-        default:
+        }
+        default:{
           console.log("Error "+e);
+        }
       }
     }
   };
   const handleMouseUp = (e)=>{
     e.preventDefault();
-    setClicked(true);
     if (typeof e === 'object') {
       switch (e.button) {
         case 0:
@@ -63,12 +67,14 @@ function App() {
           console.log('Right button clicked.');
           setRightClicked(false);
           break;
-          case 3:
-            console.log('Backward button clicked.');
-            break;
-          case 4:
-            console.log('Forward button clicked.');
-            break;
+        case 3:
+          console.log('Backward button clicked.');
+          setBackwardClicked(false);
+          break;
+        case 4:
+          console.log('Forward button clicked.');
+          setForwardClicked(false);
+          break;
         default:
           console.log("Error "+e);
       }
@@ -81,23 +87,23 @@ function App() {
       case 100:
         setWheelDown(true);
         console.log("Down");
-        setClicked(false);
-        setTimeout(() => { setWheelDown(false); setClicked(true);}, 250);
+        setTimeout(() => { setWheelDown(false);}, 250);
         break;
       case -100:
         console.log("Up");
         setWheelUp(true);
-        setClicked(false);
-        setTimeout(() => {setWheelUp(false);setClicked(true);}, 250);
+        setTimeout(() => {setWheelUp(false);}, 250);
         break;
       default:
         console.log(e);
     }
    
-  }
+  };
+
   
-  
-  useEffect(() => {
+ 
+  useEffect((e) => {
+    
     window.addEventListener('mousedown', handleMouseDown);
     window.addEventListener('mouseup', handleMouseUp);
     window.addEventListener("wheel",handleMouseWheel);
@@ -109,26 +115,24 @@ function App() {
       window.removeEventListener('mouseup', handleMouseUp);
       window.removeEventListener("wheel",handleMouseWheel);
     };
-  }, []); 
-
+  },); 
+  
+  
   return (
     <div  className="container">
-      
-      {wheelUp?<img src={MouseUp} alt="mouse middle clicked"/>:[]}
-      {wheelDown?<img src={MouseDown} alt="mouse middle clicked"/>:[]}
-     
-
-      
-      {rightClicked?<img src={MouseRight} alt="mouse right clicked"/>:[]} 
-      {leftClicked?<img src={MouseLeft} alt="mouse left clicked"/>:[]} 
-      {middleClicked?<img src={MouseMiddle} alt="mouse middle clicked"/>:[]}
-     {clicked?<img src={Mouse} alt="Mouse"/>:[]} 
-      
+    
+     <SvgLoader path={MouseSVG}>
+     {leftClicked?<SvgProxy selector="#left"  stroke="cyan" />:<SvgProxy selector="#left" stroke='#000'/>}
+     {rightClicked?<SvgProxy selector="#right"  stroke="cyan" />:<SvgProxy selector="#right" stroke='#000'/>}
+     {middleClicked?<SvgProxy selector="#scroll"  fill="cyan" />:<SvgProxy selector="#scroll" fill='#000'/>}
+     {forwardClicked?<SvgProxy selector="#forward"  fill="cyan" />:<SvgProxy selector="#forward" fill='#000'/>}
+     {backwardClicked?<SvgProxy selector="#backward"  fill="cyan" />:<SvgProxy selector="#backward" fill='#000'/>}
+     {wheelUp?<SvgProxy selector="#wheelup"  stroke="cyan" />:<SvgProxy selector="#wheelup" stroke='#000'/>}
+     {wheelDown?<SvgProxy selector="#wheeldown"  stroke="cyan" />:<SvgProxy selector="#wheeldown" stroke='#000'/>}
+     </SvgLoader>
       
       </div>
   );
 }
-
-
 
 export default App;
